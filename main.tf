d2ba5453d300dd77d9c8cccc6c5fb33cc6f07bab
wcastro2868 <124313007+wcastro2868@users.noreply.github.com>
2023-06-27 16:57:13 -0400

    Edit on vpc, instnace, and sg

--- main.tf
+++ main.tf
@@ -32,9 +32,9 @@ module "vpc" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -45,13 +45,13 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog_new"
+  name    = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.vpc_id
 
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_rules       = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
