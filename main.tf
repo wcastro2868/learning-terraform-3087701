baed63e04a511a389ba756a2255035a15905c3b0
wcastro2868 <124313007+wcastro2868@users.noreply.github.com>
2023-06-27 18:00:15 -0400

    false nat

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "blog_vpc" {
   azs             = ["us-east-1a","us-east-1b","us-east-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
 
   tags = {
     Terraform   = "true"
